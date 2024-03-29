1f55a61a01cd5fa98b22d800a7b316118036f001
Kam <KamronGaming@gmail.com>
2024-03-29 02:28:13 -0400

    Pick proper Client and Server side packets

--- src/main/java/com/goodbird/npcgecko/network/NetworkHandler.java
+++ src/main/java/com/goodbird/npcgecko/network/NetworkHandler.java
@@ -1,6 +1,7 @@
 package com.goodbird.npcgecko.network;
 
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
@@ -9,21 +10,30 @@ import net.minecraft.entity.player.EntityPlayerMP;
 
 public final class NetworkHandler {
     private static final SimpleNetworkWrapper wrapper = new SimpleNetworkWrapper("npcgecko");
+    private static Side side = FMLCommonHandler.instance().getSide();
 
     public static void init() {
+        if(side.isServer())
+            return;
         wrapper.registerMessage(CPacketSyncAutoAnim.class, CPacketSyncAutoAnim.class, 0, Side.CLIENT);
         wrapper.registerMessage(CPacketSyncManualAnim.class, CPacketSyncManualAnim.class, 1, Side.CLIENT);
     }
 
     public static void sendToPlayer(IMessage message, EntityPlayer player) {
+        if(side.isClient())
+            return;
         wrapper.sendTo(message, (EntityPlayerMP) player);
     }
 
     public static void sendToAll(IMessage message) {
+        if(side.isClient())
+            return;
         wrapper.sendToAll(message);
     }
 
     public static void sendToServer(IMessage message) {
+        if(side.isServer())
+            return;
         wrapper.sendToServer(message);
     }
 }
